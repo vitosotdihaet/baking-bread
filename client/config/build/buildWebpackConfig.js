3a99bce972f7a00e7a0c05a08cb828cfb5db79ca
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-23 00:48:15 +0300

    Config: configure webpack for next

--- client/config/build/buildWebpackConfig.js
+++ client/config/build/buildWebpackConfig.js
@@ -7,20 +7,9 @@ const buildWebpackConfig = (
      */
     config,
 ) => {
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-    const fileLoader = {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-    };
-
-    config.module.rules.push(fileLoader);
-    buildRules(config);
     buildPlugins(config);
 
     config.resolve.mainFiles.push('index');
-
-    fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
 };
