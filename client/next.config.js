6529852ed3360f79fe6b9ff3543dcf94b8b6785d
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-23 00:49:04 +0300

    Config: extend next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,11 @@
-const buildWebpackConfig = require('./config/build/buildWebpackConfig');
 // const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer');
 const {
     PHASE_DEVELOPMENT_SERVER,
     PHASE_PRODUCTION_BUILD,
 } = require('next/dist/shared/lib/constants');
+const buildWebpackConfig = require('./config/build/buildWebpackConfig');
+const buildSecurityHeaders = require('./config/next/buildSecurityHeaders');
 
 withBundleAnalyzer({
     enabled: process.env.ANALYZE === 'true',
@@ -14,10 +15,17 @@ module.exports = async (phase, { defaultConfig }) => {
     const isDev = phase === PHASE_DEVELOPMENT_SERVER;
     const isProd = phase === PHASE_PRODUCTION_BUILD;
 
+    const securityHeaders = buildSecurityHeaders();
+
     /**
      * @type {import('next').NextConfig}
      */
     const nextConfig = {
+        i18n: {
+            locales: ['ru-RU', 'en-US', 'nl-NL'],
+            defaultLocale: 'en-US',
+            localeDetection: false,
+        },
         compress: true,
         distDir: 'build',
         poweredByHeader: false,
@@ -25,6 +33,10 @@ module.exports = async (phase, { defaultConfig }) => {
         httpAgentOptions: {
             keepAlive: true,
         },
+        experimental: {
+            appDir: false,
+        },
+        headers: securityHeaders,
         devIndicators: {
             buildActivity: true,
             buildActivityPosition: 'bottom-right',
@@ -34,7 +46,5 @@ module.exports = async (phase, { defaultConfig }) => {
     };
     // TODO: READ ABOUT NEXT ESLINT AND STORYBOOK RULES
     // TODO: READ MORE ABOUT PHASES OF BUILD
-    // TODO: EDIT VARIABLES HERE
-    // TODO: FIX IT
     return withBundleAnalyzer({}, nextConfig);
 };
