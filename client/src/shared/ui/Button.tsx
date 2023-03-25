c312b90a4bbc1ee3e8484bef29f479556c94b1d0
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-25 15:04:29 +0300

    Shared: create first ui component

---
+++ client/src/shared/ui/Button.tsx
@@ -0,0 +1,49 @@
+import {
+    ButtonHTMLAttributes, FC, memo, ReactNode,
+} from 'react';
+
+export enum ButtonTheme {
+    CLEAR = 'clear',
+    CLEAR_INVERTED = 'clearInverted',
+    OUTLINE = 'outline',
+    OUTLINE_RED = 'outline_red',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export enum ButtonSize {
+    M = 'size_m',
+    L = 'size_l',
+    XL = 'size_xl',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string;
+    theme?: ButtonTheme;
+    square?: boolean;
+    size?: ButtonSize;
+    disabled?: boolean;
+    children?: ReactNode;
+}
+
+export const Button: FC<ButtonProps> = memo((props: ButtonProps) => {
+    const {
+        className,
+        children,
+        theme = ButtonTheme.OUTLINE,
+        square,
+        size = ButtonSize.M,
+        disabled,
+        ...otherProps
+    } = props;
+
+    return (
+        <button
+            type='button'
+            disabled={disabled}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+});
