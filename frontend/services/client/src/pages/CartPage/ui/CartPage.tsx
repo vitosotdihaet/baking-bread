8e461b3ce62261b7d72458271b880e849ef27a31
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-26 12:26:22 +0300

    App: create main pages

--- frontend/services/client/src/pages/CartPage/ui/CartPage.tsx
+++ frontend/services/client/src/pages/CartPage/ui/CartPage.tsx
@@ -1,10 +1,6 @@
 import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import Bread from 'shared/assets/icons/bread.svg';
-import { Button } from 'shared/ui/Button';
 import { useTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
 
 interface CartPageProps {
     className?: string;
@@ -12,15 +8,11 @@ interface CartPageProps {
 
 const CartPage: FC<CartPageProps> = (props) => {
     const { className } = props;
-    const { t, i18n } = useTranslation();
-    const router = useRouter();
+    const { t } = useTranslation();
 
     return (
         <div className={classNames('', {}, [className])}>
-            {t('HomePage')}
             {t('CartPage')}
-            <Link href='/'>{t('Go back')}</Link>
-            <Bread style={{ fill: 'brown' }} />
         </div>
     );
 };
--- frontend/services/client/src/pages/HomePage/ui/HomePage.tsx
+++ frontend/services/client/src/pages/HomePage/ui/HomePage.tsx
@@ -1,11 +1,6 @@
 import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import Bread from 'shared/assets/icons/bread.svg';
-import { Button } from 'shared/ui/Button';
 import { useTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-// import cls from './HomePage.module.scss';
 
 interface HomePageProps {
     className?: string;
@@ -13,14 +8,11 @@ interface HomePageProps {
 
 const HomePage: FC<HomePageProps> = (props) => {
     const { className } = props;
-    const { t, i18n } = useTranslation();
-    const router = useRouter();
+    const { t } = useTranslation();
 
     return (
         <div className={classNames('', {}, [className])}>
             {t('HomePage')}
-            <Link href='/cart'>{t('Go to cart')}</Link>
-            <Bread style={{ fill: 'brown' }} />
         </div>
     );
 };
