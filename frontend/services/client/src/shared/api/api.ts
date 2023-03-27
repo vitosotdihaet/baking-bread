7331836517b9f47d2348e8517b4e9f539cd6b88c
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-27 18:58:34 +0300

    Shared: update api

--- frontend/services/client/src/shared/api/api.ts
+++ frontend/services/client/src/shared/api/api.ts
@@ -3,3 +3,10 @@ import axios from 'axios';
 export const $api = axios.create({
     baseURL: process.env.API_URL,
 });
+
+$api.interceptors.request.use((config) => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+});
