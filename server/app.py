4350ef918efbc7055cc935897589e4c552c67c3c
eugenedzh <toropkibn@gmail.com>
2023-03-28 16:59:42 +0300

    added cookies tokens, updated auth.py, deprecated test_signup.py

--- server/app.py
+++ server/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
-from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 import json
 from datetime import timedelta
@@ -14,16 +14,24 @@ def create_app(db_name, password):
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://postgres:{password}@localhost/{db_name}' # authorizing the database (only on localhost now)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.from_envvar('ENV_FILE_LOCATION')
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(seconds=10)
-    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(minutes=1)
+
+    app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+    app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
+    app.config['JWT_REFRESH_COOKIE_PATH'] = '/api/'
+    app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+    app.config['JWT_COOKIE_SECURE'] = True
+
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=5)
+    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
 
     # Initialize extensions
     # To use the application instances above, instantiate with an application:
+
     db.init_app(app)
     migrate.init_app(app, db)
     ma.init_app(app)
-    cors.init_app(app)
     jwt.init_app(app)
+    cors.init_app(app)
 
     return app
 
@@ -31,8 +39,8 @@ def create_app(db_name, password):
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
-cors = CORS()
 jwt = JWTManager()
+cors = CORS()
 
 with open("local_db_info.json") as ldi:
     info = json.load(ldi)
--- server/routes/auth.py
+++ server/routes/auth.py
@@ -2,13 +2,19 @@ from functools import wraps
 
 from app import db, app, jwt
 from flask import abort, jsonify, request, redirect, url_for
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, create_refresh_token, get_jwt, verify_jwt_in_request
+from flask_jwt_extended import (
+    jwt_required, create_access_token,
+    create_refresh_token, get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies, verify_jwt_in_request, get_jwt
+)
 from sqlalchemy.exc import IntegrityError
 from models import User
 
 
-class InvalidApi(Exception): # editable class template for all kinds of errors
-    status_code = 400 # default status code, if it's not provided
+# ERROR HANDLING PART
+
+class ApiError(Exception): # editable class template for all kinds of errors
+    status_code = 400 # default status code 'BAD_REQUEST', if it's not provided
 
 
     def __init__(self, error, status_code=None):
@@ -20,50 +26,53 @@ class InvalidApi(Exception): # editable class template for all kinds of errors
             self.status_code = status_code
 
 
-    def to_dict(self):
+    def to_dict(self): # returning json response with a name of an error
         for_json = dict()
         for_json['Message'] = self.error
         
         return for_json
 
 
-@app.errorhandler(InvalidApi) # errorhandler for @app.routes for all kinds of errors (executes with 'raise InvalidApi({some data})')
+@app.errorhandler(ApiError) # errorhandler for @app.routes for all kinds of errors (executes with `raise ApiError({some data})`)
 def error_response_callback(error):
     return jsonify(error.to_dict()), error.status_code
 
 
 @jwt.invalid_token_loader
 def invalid_token_callback(error_string):
-    return jsonify(Error='INVALID_TOKEN'), 422
+    return jsonify(Message='INVALID_TOKEN'), 422
 
 
 @jwt.expired_token_loader
 def expired_token_callback(jwt_header, jwt_payload):
-    return jsonify(Error='TOKEN_HAS_EXPIRED'), 401
+    return jsonify(Message='TOKEN_HAS_EXPIRED'), 401
 
 
 @jwt.unauthorized_loader
 def unauthorized_callback(error_string):
-    return jsonify(Error='NO_TOKEN_PROVIDED'), 401
+    return jsonify(Message='NO_TOKEN_PROVIDED'), 401
 
 
 @jwt.needs_fresh_token_loader
 def no_fresh_token_callback(jwt_header, jwt_payload):
-    return jsonify(Error='NO_FRESH_TOKEN_PROVIDED'), 401
+    return jsonify(Message='NO_FRESH_TOKEN_PROVIDED'), 401
 
+
+# AUTHENTIFICATION PART
 
 # Here is a custom decorator that verifies the token is present in the request,
-# as well as insuring that the tpken has a claim indicating that this user is
+# as well as insuring that the token has a claim indicating that this user is
 # an administrator
 def admin_required():
     def wrapper(fn):
 
         @wraps(fn)
         def decorator(*args, **kwargs):
+
             verify_jwt_in_request()
             claims = get_jwt()
 
-            if claims["is_administrator"]:
+            if claims['is_administrator']:
                 return fn(*args, **kwargs)
             else:
                 return jsonify(Error='ADMIN_RIGHTS_REQUIRED'), 403
@@ -73,75 +82,153 @@ def admin_required():
     return wrapper
 
 
-@app.route('/signup', methods=['POST'])
+@app.route('/api/signup', methods=['POST'])
 def signup_post():
+
     username = request.json.get('username')
     password = request.json.get('password')
 
     if username is None or password is None: # check for missing arguments
-        raise InvalidApi('LACK_OF_SINGUP_DATA')
+        raise ApiError('LACK_OF_SINGUP_DATA')
+
+    user = db.session.query(User).filter_by(username=username).first()
+
+    if user is not None:
+        raise ApiError('USER_ALREADY_EXISTS', status_code=409)
+
 
     user = User(username=username)
     user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
 
-    try: # check if user already exists
-        db.session.add(user)
-        db.session.commit()
-    except IntegrityError:
-        db.session.rollback()
-        raise InvalidApi('USER_ALREADY_EXISTS', status_code=409)
+    userId = str(user.id)
 
-    return "something in return"
+    response = jsonify(signup=True)
+
+    # creating access token with info of user id and admin rights and refresh token with info of user id
+    # note that here we set `fresh=True` for access to endpoint `delete-account`
+    # User will have `fresh=False` parameter in token with accessing `signup` endpoint and `refresh` endpoint
+    # so anyone who has fresh token could not do some crtitical things without fresh token, such as deleting an account
+    access_token = create_access_token(identity=userId, additional_claims={'is_administrator': False}, fresh=False)
+    refresh_token = create_refresh_token(identity=userId)
+
+    # setting access and refresh token in cookies
+    set_access_cookies(response, access_token)
+    set_refresh_cookies(response, refresh_token)
+
+    return response
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/api/login', methods=['POST'])
 def login_post():
+
     username = request.json.get('username')
     password = request.json.get('password')
 
+
     if username is None or password is None: # check for missing arguments
-        raise InvalidApi('LACK_OF_LOGIN_DATA')
+        raise ApiError('LACK_OF_LOGIN_DATA')
 
     user = db.session.query(User).filter_by(username = username).first()
 
     if user is None:
-        raise InvalidApi('INVALID_LOGIN')
+        raise ApiError('INVALID_LOGIN_OR_PASSWORD')
 
     authorized = user.verify_password(password)
 
     if not authorized:
-        raise InvalidApi('INVALID_PASSWORD')
+        raise ApiError('INVALID_LOGIN_OR_PASSWORD')
 
-    str_user_id = str(user.id)
+
+    userId = str(user.id)
+
+    response = jsonify(login=True)
 
     # creating access token with info of user id and admin rights and refresh token with info of user id
-    access_token = create_access_token(identity=str_user_id, additional_claims={"is_administrator": False})
-    refresh_token = create_refresh_token(identity=str_user_id)
+    access_token = create_access_token(identity=userId, additional_claims={'is_administrator': False}, fresh=True)
+    refresh_token = create_refresh_token(identity=userId)
 
-    return jsonify(access_token=access_token, refresh_token=refresh_token)
+    # setting access and refresh token in cookies
+    set_access_cookies(response, access_token)
+    set_refresh_cookies(response, refresh_token)
+
+    return response
+
+
+@app.route('/api/logout', methods=['POST'])
+def logout_post():
+
+    response = jsonify({'logout': True})
+
+    unset_jwt_cookies(response)
+
+    return response
+
+
+@app.route('/api/delete-account', methods=['DELETE'])
+@jwt_required(fresh=True)
+def delete_account():
+
+    identity = get_jwt_identity()
+
+    user = db.session.query(User).filter_by(id = identity).first()
+
+    if user is None:
+        raise ApiError('USER_DOESNT_EXIST')
+
+    db.session.delete(user)
+    db.session.commit()
+
+    response = jsonify({'deleted_account': True})
+    unset_jwt_cookies(response)
+
+    return response
 
 
 # We are using the `refresh=True` options in jwt_required to only allow
 # refresh tokens to access this route.
-@app.route("/refresh", methods=["POST"])
+@app.route('/api/refresh', methods=['POST'])
 @jwt_required(refresh=True)
-def refresh():
+def refresh_token():
+
     identity = get_jwt_identity() # getting id of our user from token
+    access_token = create_access_token(identity=identity, additional_claims={'is_administrator': False}, fresh=False)
 
-    access_token = create_access_token(identity=identity, additional_claims={"is_administrator": False})
+    response = jsonify({'refresh': True})
+    set_access_cookies(response, access_token)
 
-    return jsonify(access_token=access_token)
+    return response
 
 
-# checks via @jwt_required decorator if user has logged in 
-@app.route("/user-access", methods=["GET"])
+# checks via @jwt_required decorator if user has access-token
+@app.route('/api/user-access', methods=['GET'])
 @jwt_required()
 def user_protected():
-    return jsonify(Access='approved')
+
+    identity = get_jwt_identity()
+
+    return jsonify(Access='approved', Identity=identity)
 
 
 # checks via @admin_required decorator if user has admin rights
-@app.route("/admin-access", methods=["GET"])
+@app.route('/api/admin-access', methods=['GET'])
 @admin_required()
 def admin_protected():
-    return jsonify(Access='approved')
+
+    identity = get_jwt_identity()
+
+    return jsonify(Access='approved', Identity=identity)
+
+
+# all users have access to this endpont (no access token required)
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify(Hello='world')
+
+
+# # this decorator is mostly used for debugging
+# # In that example we print all cookies provided in request before every request
+# @app.before_request
+# def before_request_func():
+#     print(request.cookies)
