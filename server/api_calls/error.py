8a4cd92a3305bf23f469aee291dbe9b3de9bed3c
vitosotdihaet <vitostikitos@gmail.com>
2023-03-29 00:11:18 +0300

    Move basic error handler to api_calls/error.py

--- server/api_calls/error.py
+++ server/api_calls/error.py
@@ -1,3 +1,7 @@
+from app import app
+from flask import jsonify
+
+
 class ApiError(Exception): # editable class template for all kinds of errors
     status_code = 400 # default status code 'BAD_REQUEST', if it's not provided
 
@@ -16,3 +20,7 @@ class ApiError(Exception): # editable class template for all kinds of errors
         for_json['Message'] = self.error
         
         return for_json
+
+@app.errorhandler(ApiError) # handles all kinds of errors (executes with `raise ApiError({some data})`)
+def error_response_callback(error):
+    return jsonify(error.to_dict()), error.status_code
