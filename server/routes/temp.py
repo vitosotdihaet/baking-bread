66556bb6199802273d6c2d91139141d51d5bf7a7
eugenedzh <toropkibn@gmail.com>
2023-04-09 22:14:51 +0300

    temp.py form-data

--- server/routes/temp.py
+++ server/routes/temp.py
@@ -1,8 +1,11 @@
 from app import app
-from flask import jsonify
+from flask import request
 
+# import markdown
+# import markdown.extensions.fenced_code
+# from pygments.formatters import HtmlFormatter
 
 # all users have access to this endpont (no access token required)
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['POST'])
 def test():
-    return jsonify(Hello='world')
+    return request.files['file'].read() # response with a file from a request containing form-data with this file
