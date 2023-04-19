6c5117cd11855ad88a284534c8f675c7d00119d9
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-19 14:43:12 +0300

    Scripts: complete slice generators

--- frontend/services/client/scripts/createSlice/index.js
+++ frontend/services/client/scripts/createSlice/index.js
@@ -1,7 +1,15 @@
-const createTemplate = require('./templates/createTemplate');
+const minimist = require('minimist');
+const createTemplate = require('./generators/createTemplate');
 
 const layer = process.argv[2];
 const sliceName = process.argv[3];
+
+const args = minimist(process.argv.slice(2), {
+    alias: {
+        api: 'api',
+        model: 'model',
+    },
+});
 
 const layers = ['features', 'entities', 'pages'];
 
@@ -13,4 +21,4 @@ if (!sliceName) {
     throw new Error('Укажите название слайса');
 }
 
-createTemplate(layer, sliceName);
+createTemplate(layer, sliceName, args);
