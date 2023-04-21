6926d44e9177ee8b05186e45733010f6273269d3
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-21 04:00:47 +0300

    Provider: add store provider for test by conditiona

--- frontend/services/client/src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ frontend/services/client/src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,17 +1,41 @@
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import { FC, ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { AppStore } from '../config/store';
+import { StateSchema } from '../config/StateSchema';
+import { AppStore, createReduxStore } from '../config/store';
 
 interface StoreProviderProps {
     children: ReactNode;
-    initialStore: AppStore;
+    initialStore?: AppStore;
+    initialState?: DeepPartial<StateSchema>;
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
 }
 
 export const StoreProvider: FC<StoreProviderProps> = (props) => {
     const {
         children,
         initialStore,
+        initialState,
+        asyncReducers,
     } = props;
+
+    if (__PROJECT__ === 'storybook') {
+        console.log(__PROJECT__);
+        const store = createReduxStore(
+            initialState as StateSchema,
+            asyncReducers as ReducersMapObject<StateSchema>,
+        );
+
+        return (
+            <Provider store={store}>
+                {children}
+            </Provider>
+        );
+    }
+
+    if (!initialStore) {
+        return null;
+    }
 
     return (
         <Provider store={initialStore}>
