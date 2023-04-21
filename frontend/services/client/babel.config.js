951c429efa5894996ee078dbf289d361ff9ce54e
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-21 04:06:34 +0300

    Complete first page

--- frontend/services/client/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        'next/babel',
-        '@babel/preset-typescript',
-    ],
-    // plugins: ['inline-react-svg'],
-};
