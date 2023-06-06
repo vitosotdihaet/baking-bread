8d5e220d0d3640318b31cc8654a59c5960d96459
eugenedhz <toropkibn@gmail.com>
2023-06-06 16:55:22 +0300

    Delete: unnecessary files

--- server/src/services/admin.py
+++
@@ -1,8 +0,0 @@
-from server.src.services.user import User
-
-class Admin(User):
-    def __init__(self) -> None:
-        super().__init__()
-
-    # def set_permission(self, other, perm):
-    #     db.set(self.id, other.id, perm)
