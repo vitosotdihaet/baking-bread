f13a2fea34aee98f8487d92fd2d90f11153ae1b5
eugenedhz <toropkibn@gmail.com>
2023-06-07 12:57:49 +0300

    Add: error regidtering for 404 status code

--- server/api/error/error_register.py
+++ server/api/error/error_register.py
@@ -8,6 +8,11 @@ def error_response_callback(error):
     return jsonify(error.to_dict()), error.status_code
 
 
+@app.errorhandler(404)
+def error_response_callback(error):
+    return jsonify(message='RESOURCE_NOT_FOUND'), 404
+
+
 @app.errorhandler(405)
 def error_response_callback(error):
     return jsonify(message='METHOD_NOT_ALLOWED'), 405
